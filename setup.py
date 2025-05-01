f3391defd600e688f20a147d36c2ad448ff429ff
hqdem <ilyadem4@gmail.com>
2025-05-01 22:19:24 +0300

    add library for pattern prediction

--- setup.py
+++ setup.py
@@ -7,11 +7,17 @@ def parse_requirements(filename):
 setup(
     name="chatcluster",
     version="0.1.0",
-    description="Классификация и кластеризация паттернов общения",
-    author="Твоё Имя",
+    description="Классификация паттернов общения",
+    author="iademidov",
+    author_email="iademidov@edu.hse.com",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=parse_requirements("requirements.txt"),
     python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # или другой
+        "Operating System :: OS Independent",
+    ],
 )
